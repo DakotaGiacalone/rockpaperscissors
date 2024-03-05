4670049b58b36162ee41a34e31a09a2d336e8fdb
DakotaGiacalone <Killjoy199010@gmail.com>
2024-03-05 14:36:07 -0500

    Added basic gui

--- script.js
+++ script.js
@@ -1,59 +1,126 @@
-let userScore = 0
-let computerScore = 0
+const userResponse = document.querySelector('#userResponse');
+const results = document.querySelector('#results');
+const roundResponse = document.createElement('p');
+roundResponse.setAttribute('id', 'roundResponse')
+const playerScoreText = document.createElement('p');
+playerScoreText.setAttribute('id', 'playerScoreText');
+const computerScoreText = document.createElement('p');
+computerScoreText.setAttribute('id', 'computerScoreText');
+const endGame = document.querySelector('#endGame');
+results.appendChild(playerScoreText);
+results.appendChild(computerScoreText);
+results.appendChild(roundResponse);
 
-function computerPlay() {
-    let rps = ["rock", "paper", "scissors"];
-    let random = rps[Math.floor(Math.random() * rps.length)];
-    return random;
+let playerScore = 0;
+let computerScore = 0;
+
+userResponse.addEventListener('click', (event) => {
+  const response = event.target;
+  const computerChoice = getComputerChoice();
+  const playerSelection = response.id;
+  const roundResult = playRound(playerSelection, computerChoice);
+
+  if (roundResult > 0) {
+    playerScore += 1;
+  } else if (roundResult < 0) {
+    computerScore += 1;
   }
-  
-  console.log(computerPlay())
 
-  function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return ("It's a draw!");
-      } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        userScore += 1;
-        return ("You win! Rock beats scissors");
-      } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1;
-        return ("You lose! Paper beats rock");
-      } else if (playerSelection === "paper" && computerSelection === "rock") {
-        userScore += 1;
-        return ("You win! Paper beats rock");
-      } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore += 1;
-        return ("You lose! Scissors beat paper");
-      } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        userScore += 1;
-        return ("You win! Scissors beat paper");
-      } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1;
-        return ("You lose!Rock beats scissors");
-      }
-    
+  playerScoreText.textContent = `Player: ${playerScore}`;
+  computerScoreText.textContent = `Computer ${computerScore}`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    userResponse.removeChild(rock);
+    userResponse.removeChild(paper);
+    userResponse.removeChild(scissors);
+
+    playerScoreText.textContent = '';
+    computerScoreText.textContent = '';
+    roundResponse.textContent = '';
+
+    const endGameText = document.createElement('h2');
+    endGame.appendChild(endGameText);
+    if (playerScore === 5) {
+      endGameText.textContent = 'Congratulations! You win the game!';
+    } else {
+      endGameText.textContent = 'Game over. You lose!';
     }
 
-    function game() {
-        for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What is your move? Rock, Paper or Scissors?").toLowerCase();
-        let computerSelection = computerPlay().toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("Player: " + userScore);
-        console.log("Computer: " +computerScore);
-    
-        }
-    }
-    
+    const restartBtn = document.createElement('button');
+    endGame.appendChild(restartBtn);
+    restartBtn.textContent = 'Play Again';
+    restartBtn.addEventListener('click', () => {
+      restartBtn.remove();
+      endGameText.remove();
+      playerScore = 0;
+      computerScore = 0;
+      const rockBtn = document.createElement('button');
+      rockBtn.setAttribute('id', 'rock');
+      rockBtn.textContent = 'Rock';
+      userResponse.appendChild(rockBtn);
+      const paperBtn = document.createElement('button');
+      paperBtn.setAttribute('id', 'paper');
+      paperBtn.textContent = 'Paper';
+      userResponse.appendChild(paperBtn);
+      const scissorsBtn = document.createElement('button');
+      scissorsBtn.setAttribute('id', 'scissors');
+      scissorsBtn.textContent = 'Scissors';
+      userResponse.appendChild(scissorsBtn);
 
-    game();
+    })
+  }
+});
 
-    if (userScore == computerScore) {
-        console.log(`Game Result: Ugh it's a Tie! ${userScore} to ${computerScore}`);
+
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  let computerChoice;
+  switch (randomNumber) {
+    case 0:
+      computerChoice = "rock"
+      return computerChoice
+    case 1:
+      computerChoice = "paper"
+      return computerChoice
+    default:
+      computerChoice = "scissors"
+      return computerChoice
+  }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+  let winner;
+
+
+  if (playerSelection === computerSelection) {
+    winner = "";
+    roundResponse.textContent = "It's a tie.";
+    return 0;
+  } else if (playerSelection === "rock" || computerSelection === "rock") {
+    if (playerSelection === "paper" || computerSelection === "paper") {
+      winner = "paper"
+    } else {
+      winner = "rock"
     }
-    else if (userScore > computerScore) {
-        console.log(`Game Result: Congrats! You win! ${userScore} to ${computerScore}`);
+  } else if (playerSelection === "paper" || computerSelection === "paper") {
+    if (playerSelection === "scissors" || computerSelection === "scissors") {
+      winner = "scissors"
     }
-    else {
-        console.log(`Game Result: You lose! ${computerScore} to ${userScore}`);
-    }
+  }
+
+  if (winner === playerSelection) {
+    roundResponse.textContent = `You win this round! ${capitalize(winner)} beats ${capitalize(computerSelection)}`;
+    return 1;
+  } else {
+    roundResponse.textContent = `You lose this round! ${capitalize(winner)} beats ${capitalize(playerSelection)}`;
+    return -1;
+  }
+}
+
+function capitalize(string) {
+  const firstLetter = string.at(0);
+  const capitalizedLetter = firstLetter.toUpperCase();
+  return string.replace(firstLetter, capitalizedLetter)
+}
